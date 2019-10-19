780ce74ac01b54addf94effe9192777fca9966a8
SpicyIslandFruits <spicyisland@gmail.com>
2019-10-19 17:40:06 +0900

    App.jsの修正

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import {withAuthenticator} from 'aws-amplify-react';
 import API, {graphqlOperation} from '@aws-amplify/api'
 import PubSub from '@aws-amplify/pubsub';
 import {createMessage} from './graphql/mutations'
 
-import Amplify, {Auth} from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import config from './aws-exports'
 // other imports
@@ -19,38 +19,39 @@ const initialState = {messages: []};
 const reducer = (state, action) => {
     switch (action.type) {
         case 'QUERY':
-            return {...state, messages: action.messages}
+            return {...state, messages: action.messages};
         case 'SUBSCRIPTION':
-            return {...state, messages: [...state.messages, action.message]}
+            return {...state, messages: [...state.messages, action.message]};
         default:
             return state
     }
-}
+};
 
-API.configure(config)             // Configure Amplify
+API.configure(config);             // Configure Amplify
 PubSub.configure(config);
 
 async function createNewMessage() {
-    const message = {owner: "Owner", content: "Content"}
+    const message = {content: "Content"};
     await API.graphql(graphqlOperation(createMessage, {input: message}))
 }
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
-        getData()
-        const subscription = API.graphql(graphqlOperation(onCreateMessage)).subscribe({
+        getData();
+        // change owner variable to logged in username.
+        const subscription = API.graphql(graphqlOperation(onCreateMessage, {owner: "Admin"})).subscribe({
             next: (eventData) => {
                 const message = eventData.value.data.onCreateMessage;
                 dispatch({type: 'SUBSCRIPTION', message})
             }
-        })
+        });
         return () => subscription.unsubscribe()
-    }, [])
+    }, []);
 
     async function getData() {
-        const messageData = await API.graphql(graphqlOperation(listMessages))
+        const messageData = await API.graphql(graphqlOperation(listMessages));
         dispatch({type: 'QUERY', messages: messageData.data.listMessages.items});
     }
 
@@ -64,4 +65,4 @@ function App() {
     );
 }
 
-export default App;
+export default withAuthenticator(App);
